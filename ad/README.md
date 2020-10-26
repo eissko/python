0a2a1c835f00fa33b9a283a5c42b4716c58274cb
eissko <eissko@blackhole>
2020-10-26 13:38:43 +0000

    ldap query from python using ldap3 & ntlm auth

---
+++ ad/README.md
@@ -0,0 +1,20 @@
+### Install Ldap
+```
+sudo apt-get install libsasl2-dev python-dev libldap2-dev libssl-dev
+```
+```
+pip3 install python-ldap
+```
+
+### better is to use ldap 3
+- https://pypi.org/project/ldap3/
+
+```
+python -m pip install ldap3
+```
+
+### Ldap3 reference
+- https://www.python-ldap.org/en/python-ldap-3.3.0/reference/ldap.html#example
+- https://ldap3.readthedocs.io/en/latest/searches.html
+
+
---
+++ ad/activedirectory.py
@@ -0,0 +1,29 @@
+import yaml
+from ldap3 import Server, Connection, SUBTREE, SIMPLE, SYNC, ALL, SASL, NTLM
+
+# load parameters
+import yaml
+config = yaml.safe_load(open(".config.yml"))
+
+
+
+# define the server and the connection
+s = Server(config['server'], get_info=ALL)
+c = Connection(s, user=config['username'], password=config['password'], authentication=NTLM)
+# perform the Bind operation
+if not c.bind():
+    print('error in bind', c.result)
+else:
+    print('connection is succesfull', c.result)
+
+c.search(search_base = config['search_base'],
+         search_filter = '(&(objectClass=computer)(CN={}))'.format(config['computer_name']),
+         search_scope = SUBTREE,
+         attributes = ['cn', 'distinguishedName'],
+         paged_size = 1)
+
+for entry in c.response:
+    if 'dn' in entry:
+        print(entry)
+    else:
+        print(entry)
